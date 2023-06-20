e73d63a366423fb11f101f1fec7ccc3d5ab82c61
cleamwafers <cleamwafers@yahoo.co.jp>
2023-06-20 22:52:43 +0900

    fin_p62

--- chapter03/ColorPicker/js/script.js
+++ chapter03/ColorPicker/js/script.js
@@ -1,2 +1,6 @@
 console.log(document.querySelector('#colorPicker').value);
 document.querySelector('#colorText').textContent ='カラーコード:';
+/*<h1>を代入('')に入れてしまうと文字列として表示されてしまう。 */
+/*HTMLタグも含めたいならinnerHTMLが使える
+document.querySelector('#colorText').innerHTML = '<h1>カラーコード</h1>'
+*/
