e5f97ce4b9ba87a097eec0537562363a2d913431
cleamwafers <cleamwafers@yahoo.co.jp>
2023-06-20 23:29:22 +0900

    fin_chapter03_6

--- chapter03/ColorPicker/js/script.js
+++ chapter03/ColorPicker/js/script.js
@@ -1,6 +1,7 @@
 console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent = 'カラーコード:' + document.querySelector('#colorPicker').value;
+document.querySelector('#colorText').textContent = `カラーコード:${document.querySelector('#colorPicker').value}`;
 /*<h1>を代入('')に入れてしまうと文字列として表示されてしまう。 */
 /*HTMLタグも含めたいならinnerHTMLが使える
-document.querySelector('#colorText').innerHTML = '<h1>カラーコード</h1>'
-*/
+document.querySelector('#colorText').innerHTML = '<h1>カラーコード</h1>'*/
+/*テンプレート文字列(テンプレートリテラル):最終的に表示したい内容をバックティック`で囲む。
+式や値、次節から登場する変数や定数など、文字列以外のコードは${}で囲む。 */
