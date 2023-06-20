79182890fc8892530dade8c068c355be483f5616
cleamwafers <cleamwafers@yahoo.co.jp>
2023-06-20 23:18:46 +0900

    fin_p65_仮解決法

--- chapter03/ColorPicker/js/script.js
+++ chapter03/ColorPicker/js/script.js
@@ -1,5 +1,5 @@
 console.log(document.querySelector('#colorPicker').value);
-document.querySelector('#colorText').textContent = document.querySelector('#colorPicker').value;
+document.querySelector('#colorText').textContent = 'カラーコード:' + document.querySelector('#colorPicker').value;
 /*<h1>を代入('')に入れてしまうと文字列として表示されてしまう。 */
 /*HTMLタグも含めたいならinnerHTMLが使える
 document.querySelector('#colorText').innerHTML = '<h1>カラーコード</h1>'
