9574d48d4a334fa34762195358f28b0462ddbdb3
cleamwafers <cleamwafers@yahoo.co.jp>
2023-07-21 06:10:31 +0900

    ローディング中の画面_fin

--- chapter04/02/js/script.js
+++ chapter04/02/js/script.js
@@ -3,3 +3,10 @@ const loading = document.querySelector('#loading');
 window.addEventListener('load', () => {
   loading.classList.add('loaded');
 });
+
+/*クラスの追加
+要素.classList.add('クラス名1','クラス名2','クラス名3');
+*/
+/*クラス名の削除
+要素.classList.remove('クラス名1','クラス名2','クラス名3');
+*/
